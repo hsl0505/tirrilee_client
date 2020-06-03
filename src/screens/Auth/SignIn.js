cccbbb2d54014439b7bc4381beb0e6382e2646ef
hyunsung <hslgood@gmail.com>
2020-06-04 00:58:19 +0900

    Add signIn/signUp screen

---
+++ src/screens/Auth/SignIn.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import SignInComponent from '../../components/Auth/SignInComponent';
+// import {StyleSheet} from 'react-native';
+
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: 'white',
+//     flex: 1,
+//   },
+// });
+
+function SignIn() {
+  return <SignInComponent />;
+}
+
+export default SignIn;
---
+++ src/screens/Auth/SignUp.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {
+  // StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+function SignUp() {
+  return (
+    <View>
+      <Text>SignUp screen</Text>
+    </View>
+  );
+}
+
+export default SignUp;
