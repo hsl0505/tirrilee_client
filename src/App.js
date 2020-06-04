f3d6f64a0aa3f2eb673b05d6a53be8eed1de3409
hyunsung <hslgood@gmail.com>
2020-06-05 01:24:35 +0900

    Delete useless

--- src/App.js
+++ src/App.js
@@ -10,12 +10,6 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
-// import {
-//   // StyleSheet,
-//   View,
-//   Text,
-// } from 'react-native';
-
 import InitScreen from './screens/InitScreen';
 
 const App = () => {
@@ -26,44 +20,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
 
 export default App;
