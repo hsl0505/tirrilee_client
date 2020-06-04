5d7817af61a0193e3bfeae55a3f00329c0abb649
hyunsung <hslgood@gmail.com>
2020-06-05 01:27:44 +0900

    Add navigation props and context

--- src/screens/Auth/SignIn.js
+++ src/screens/Auth/SignIn.js
@@ -1,16 +1,13 @@
-import * as React from 'react';
+import React, {useContext} from 'react';
 import SignInComponent from '../../components/Auth/SignInComponent';
-// import {StyleSheet} from 'react-native';
 
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: 'white',
-//     flex: 1,
-//   },
-// });
+import LoginContext from './LoginContext';
 
-function SignIn() {
-  return <SignInComponent />;
+function SignIn(props) {
+  const {navigation} = props;
+  const login = useContext(LoginContext);
+
+  return <SignInComponent navigation={navigation} login={login} />;
 }
 
 export default SignIn;
