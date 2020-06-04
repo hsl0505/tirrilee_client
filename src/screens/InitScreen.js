cfe776ea92cd3ca519f6b8900b715559b2ad8877
hyunsung <hslgood@gmail.com>
2020-06-05 01:27:23 +0900

    Add login context for navigation

--- src/screens/InitScreen.js
+++ src/screens/InitScreen.js
@@ -1,4 +1,6 @@
-import * as React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+// import {Image} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import BottomTabNav from './BottomTabNav';
@@ -6,23 +8,46 @@ import BottomTabNav from './BottomTabNav';
 import SignIn from './Auth/SignIn';
 import SignUp from './Auth/SignUp';
 
+import LoginContext from './Auth/LoginContext';
+
 const Switch = createStackNavigator();
 
-// // 임시 로그인
-// const isLogIn = false;
-
 function InitScreen() {
+  // 임시 로그인 state 및 함수
+  const [isLogIn, setLogIn] = useState(false);
+
   return (
-    <Switch.Navigator headerMode="none">
-      {!false ? (
-        <>
-          <Switch.Screen name="로그인" component={SignIn} />
-          <Switch.Screen name="회원가입" component={SignUp} />
-        </>
-      ) : (
-        <Switch.Screen name="메인" component={BottomTabNav} />
-      )}
-    </Switch.Navigator>
+    <LoginContext.Provider value={setLogIn}>
+      <Switch.Navigator>
+        {!isLogIn ? (
+          <>
+            <Switch.Screen
+              name="로그인"
+              component={SignIn}
+              options={{headerShown: false}}
+            />
+            <Switch.Screen
+              name="회원가입"
+              component={SignUp}
+              options={{
+                headerTitleAlign: 'center',
+                headerTitleStyle: {
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                  color: '#0379c7',
+                },
+              }}
+            />
+          </>
+        ) : (
+          <Switch.Screen
+            name="메인"
+            component={BottomTabNav}
+            options={{headerShown: false}}
+          />
+        )}
+      </Switch.Navigator>
+    </LoginContext.Provider>
   );
 }
 
