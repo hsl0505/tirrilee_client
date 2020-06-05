a887dad739d72b20e6366350a5762298ebc96747
hyunsung <hslgood@gmail.com>
2020-06-05 21:45:48 +0900

    Add community write component

---
+++ src/screens/Community/CommunityWriteScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import CommunityWriteComponent from '../../components/Community/CommunityWriteComponent';
+
+export default function CommunityWriteScreen(props) {
+  const {navigation} = props;
+  return <CommunityWriteComponent navigation={navigation} />;
+}
